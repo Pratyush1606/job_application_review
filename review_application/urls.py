5827d1e88da4160db45dd0014b11a086790d7b15
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-08-26 22:52:07 +0530

    Added urls in django app
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

---
+++ review_application/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from review_application.views import *
+
+app_name = 'review_application'
+urlpatterns = [
+
+]
--- review_application/views.py
+++ review_application/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.renderers import TemplateHTMLRenderer
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
