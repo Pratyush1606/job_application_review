bc24b7a6f798dab2a148e9dd35f964644fd79d85
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2022-08-27 14:49:11 +0530

    Configured settings
    Signed-off-by: Pratyush1606 <jaiswalpratyush2015@gmail.com>

--- job_application_review/settings.py
+++ job_application_review/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["job-application-review.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
